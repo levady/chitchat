079516b9cfede316a1cb53044bb7b9f7ff3abead
Le Vady Sinarta <levady.sinarta@gmail.com>
2012-04-16 23:28:34 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chitchat::Application.config.secret_token = 'ec57f7576d4d02e28163860ee43981b112a8e7935e0c162af90d1bef1ec1918c91a209a7147c49342a095de784fc7afbac96145950b076737ea87832139f6406'
